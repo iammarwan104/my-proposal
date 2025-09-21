0ccff7ec4705e1cd489151ee39ed5c5f11ba3fb8
Marwan <marwahpinrang6@gmail.com>
2025-09-21 14:41:51 +0800

    fix errors at build proccess 3

--- src/app/ProposalPage.tsx
+++ src/app/ProposalPage.tsx
@@ -1,4 +1,4 @@
-'''"use client";
+"use client";
 
 import { useState } from "react";
 import Snowfall from "react-snowfall";
@@ -114,4 +114,3 @@ export default function ProposalPage() {
     </div>
   );
 }
-'''
